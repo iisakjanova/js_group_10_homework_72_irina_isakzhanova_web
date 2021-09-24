3a62201ea819f6d00801b9b05d9f957698f9720a
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-09-24 14:10:04 +0600

    Implemented total count

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Grid, Paper, Typography} from "@mui/material";
+import {Button, Grid, Paper, Typography} from "@mui/material";
 import {makeStyles} from "@mui/styles";
 import {useDispatch, useSelector} from "react-redux";
 
@@ -10,12 +10,24 @@ const useStyles = makeStyles(theme => ({
     root: {
         marginBottom: "20px !important",
     },
+    paper: {
+        padding: theme.spacing(1),
+    },
+    paperInner: {
+        flexWrap: "nowrap !important",
+    },
+    info: {
+       flexGrow: 1,
+    },
     item: {
         paddingRight: theme.spacing(2),
         paddingLeft: theme.spacing(2),
     },
     itemPart: {
-        width: "30%",
+        width: "50%",
+    },
+    total: {
+        width: "100%",
     },
 }));
 
@@ -34,29 +46,50 @@ const Order = (props) => {
 
     const order = props.data;
 
+    const calculateTotal = order => {
+        return Object.keys(order).reduce((acc, key) => {
+            return acc + order[key] * dishes[key].price;
+        }, 0);
+    };
+
+    const total = calculateTotal(order) + DELIVERY_PRICE;
+
     return (
         dishes
             ?
             <Grid item className={classes.root}>
-                <Paper>
-                    <Grid container direction="column">
-                        {Object.keys(order).map(key => (
-                            <Grid key={key} container direction="row" className={classes.item}>
-                                <Typography variant="subtitle1" className={classes.itemPart}>
-                                    {order[key]} X {dishes[key].title}
-                                </Typography>
-                                <Typography  variant="subtitle1" className={classes.itemPart}>
-                                    {dishes[key].price} {CURRENCY}
-                                </Typography>
+                <Paper className={classes.paper}>
+                    <Grid container direction="row" className={classes.paperInner}>
+                        <Grid container direction="row" className={classes.info}>
+                            <Grid container direction="column">
+                                {Object.keys(order).map(key => (
+                                    <Grid key={key} container direction="row" className={classes.item}>
+                                        <Typography variant="subtitle1" className={classes.itemPart}>
+                                            {order[key]} X {dishes[key].title}
+                                        </Typography>
+                                        <Typography  variant="subtitle1" className={classes.itemPart}>
+                                            {dishes[key].price} {CURRENCY}
+                                        </Typography>
+                                    </Grid>
+                                ))}
+                                <Grid container direction="row" className={classes.item}>
+                                    <Typography variant="subtitle1" className={classes.itemPart}>
+                                        Delivery:
+                                    </Typography>
+                                    <Typography  variant="subtitle1" className={classes.itemPart}>
+                                        {DELIVERY_PRICE} {CURRENCY}
+                                    </Typography>
+                                </Grid>
                             </Grid>
-                        ))}
-                        <Grid container direction="row" className={classes.item}>
-                            <Typography variant="subtitle1" className={classes.itemPart}>
-                                Delivery:
+                        </Grid>
+                        <Grid container direction="column" xs={4}>
+                            <Typography variant="subtitle1" className={classes.total}>
+                                Order total:
                             </Typography>
-                            <Typography  variant="subtitle1" className={classes.itemPart}>
-                                {DELIVERY_PRICE} {CURRENCY}
+                            <Typography variant="subtitle1" className={classes.total}>
+                                {total} {CURRENCY}
                             </Typography>
+                            <Button variant="outlined">Complete order</Button>
                         </Grid>
                     </Grid>
                 </Paper>
@@ -64,7 +97,6 @@ const Order = (props) => {
 
             :
             null
-
     );
 };
 
